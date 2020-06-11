de9264f5384eea5ac21c70267b43a94207a3c3fd
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-06-11 21:48:38 +0100

    code update

--- App.js
+++ App.js
@@ -4,18 +4,13 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 const App = () => {
   // The state is 'name' and 'setName' is our setState method for 'name'
   const [name, setName] = useState("Yusaf");
-  const [person, setPerson] = useState({ name: "Mario", age: 40 });
 
   // Changes the name upon btn press
-  const clickHandler = () => { 
-    setName('Jason Todd');
-    setPerson({ name: "Luigi", age: 45 });
-  }
+  const clickHandler = () => name === "Yusaf" ? setName("Johnson") : setName("Yusaf");
 
   return (
     <View style={styles.container}>
       <Text>My name is {name}</Text>
-      <Text>His name is {person.name} and he's {person.age} years old</Text>
       <View style={styles.btnContainer}>
         <Button title="Update name" onPress={clickHandler} />
       </View>
