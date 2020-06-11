2fa92df7209cda6aa153f0d6361315b31eb537fc
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-06-11 20:26:23 +0100

    refactoring

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
       <Text>I'm learning React Native woo!</Text>
@@ -10,10 +10,14 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
 });
+
+export default App;
