65d1b0384c9f242368961f2bd475ed53de11f7e1
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-06-11 21:24:52 +0100

    learning about state and setState

--- App.js
+++ App.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 const App = () => {
+  // The state is 'name' and 'setName' is our setState method for 'name'
+  const [name, setName] = useState("Yusaf");
+  const [person, setPerson] = useState({ name: "Mario", age: 40 });
+
+  // Changes the name upon btn press
+  const clickHandler = () => { 
+    setName('Jason Todd');
+    setPerson({ name: "Luigi", age: 45 });
+  }
+
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.boldText}>Hello World</Text>
-      </View>
-      <View style={styles.body}>
-        <Text style={styles.boldText}>I'm learning React Native!!</Text>
-        <Text style={styles.boldText}>I'm learning React Native!!</Text>
-        <Text style={styles.boldText}>I'm learning React Native!!</Text>
+      <Text>My name is {name}</Text>
+      <Text>His name is {person.name} and he's {person.age} years old</Text>
+      <View style={styles.btnContainer}>
+        <Button title="Update name" onPress={clickHandler} />
       </View>
     </View>
   );
@@ -26,20 +33,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 
-  header: {
-    backgroundColor: 'pink',
-    padding: 20,
-  },
-
-  boldText: {
-    fontWeight: 'bold'
-  },
-
-  body: {
-    backgroundColor: 'yellow',
-    padding: 20,
-    fontWeight: 'bold'
+  btnContainer: {
+    marginTop: 20
   }
+
 });
 
 export default App;
