4593c3f846433811cbdbba48f6728b5b4fa94bb0
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-06-12 22:36:02 +0100

    Can remove people by touching them

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
 
 const App = () => {
   
@@ -13,6 +13,12 @@ const App = () => {
     { name: "Bowser", id: '7' },
   ]);
 
+  // Removes a person upon pressing
+  const pressHandler = (id) => {
+    console.log(id);
+    setPeople((prevPeople) => prevPeople.filter(person => person.id !== id));
+  }
+
   return (
     <View style={styles.container}>
       {/* Printing each person in a vertical fashion (within a scrollview) */}
@@ -21,7 +27,9 @@ const App = () => {
         keyExtractor={(item) => item.id}
         data={people}
         renderItem={({ item }) => (
-          <Text style={styles.person}>{item.name}</Text>
+          <TouchableOpacity onPress={() => pressHandler(item.id)}>
+            <Text style={styles.item}>{item.name}</Text>
+          </TouchableOpacity>
         )}
       />
     </View>
@@ -38,7 +46,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20
   },
 
-  person: {
+  item: {
     marginTop: 24,
     padding: 30,
     fontSize: 24,
