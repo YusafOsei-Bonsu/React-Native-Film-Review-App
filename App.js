56502a25d000e467b164f8f51d508b4a0f40ede0
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-06-12 22:20:32 +0100

    Flatlist

--- App.js
+++ App.js
@@ -1,30 +1,29 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 const App = () => {
   
   const [people, setPeople] = useState([
-    { name: "Shaun", key: '1' },
-    { name: "Yoshi", key: '2' },
-    { name: "Mario", key: '3' },
-    { name: "Luigi", key: '4' },
-    { name: "Peach", key: '5' },
-    { name: "Toad", key: '6' },
-    { name: "Bowser", key: '7' },
+    { name: "Shaun", id: '1' },
+    { name: "Yoshi", id: '2' },
+    { name: "Mario", id: '3' },
+    { name: "Luigi", id: '4' },
+    { name: "Peach", id: '5' },
+    { name: "Toad", id: '6' },
+    { name: "Bowser", id: '7' },
   ]);
 
   return (
     <View style={styles.container}>
       {/* Printing each person in a vertical fashion (within a scrollview) */}
-      <ScrollView>
-        { people.map((person) => {
-              return (
-                <View key={person.key}>
-                  <Text style={styles.person}>{person.name}</Text>
-                </View>
-              );
-          }) }
-      </ScrollView>
+      <FlatList
+        numColumns={2}
+        keyExtractor={(item) => item.id}
+        data={people}
+        renderItem={({ item }) => (
+          <Text style={styles.person}>{item.name}</Text>
+        )}
+      />
     </View>
   );
 }
@@ -36,16 +35,16 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     paddingTop: 40,
-    paddingHorizontal: 20,
-    // alignItems: 'center',
-    // justifyContent: 'center',
+    paddingHorizontal: 20
   },
 
   person: {
     marginTop: 24,
     padding: 30,
     fontSize: 24,
-    backgroundColor: 'pink'
+    backgroundColor: 'pink',
+    marginHorizontal: 10,
+    marginTop: 24
   }
 
 });
