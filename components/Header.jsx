7c8292dde5a3f4898ff4ddb7b98c8cf9cea9b4ad
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-06-13 11:41:20 +0100

    Extra CSS for centering and boldening 'My Todos'

--- components/Header.jsx
+++ components/Header.jsx
@@ -10,10 +10,18 @@ const Header = () => {
 }
 
 const styles = StyleSheet.create({
+    
     header: {
         height: 80,
         paddingTop: 38,
         backgroundColor: 'coral'
+    },
+
+    title: {
+        textAlign: 'center',
+        color: '#fff',
+        fontSize: 20,
+        fontWeight: 'bold'
     }
 });
 
