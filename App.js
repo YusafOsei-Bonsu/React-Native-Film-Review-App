2f8cd3e680598c5cf538c7f7ce0b44b501743fa1
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-06-15 13:54:11 +0100

    Component that enables navigation

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import * as Font from 'expo-font';
 import Home from './components/Home.jsx';
 import { AppLoading } from 'expo';
+import Navigator from './routes/HomeStack.js';
 
 const getFonts = () => Font.loadAsync({
   'nunito-regular': require('./assets/Fonts/Nunito-Regular.ttf'),
@@ -12,7 +13,7 @@ const App = () => {
   const [fontsLoaded, setFontsLoaded] = useState(false);
 
   if (fontsLoaded) {
-    return <Home/>;
+    return <Navigator />;
   } else {
     return (
       <AppLoading 
