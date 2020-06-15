8a64de63ab228aca4840a2f316969f7bcbc9289e
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-06-15 12:45:34 +0100

    CSS for Home component

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 const Home = () => {
     return (
         <View style={styles.container}>
-            <Text>Home Screen</Text>
+            <Text style={styles.titleText}>Home Screen</Text>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1, 
-        padding: 24,
-        alignItems: 'center',
-        justifyContent: 'center'
+        padding: 24
+    },
+    titleText: {
+        fontFamily: 'nunito-bold',
+        fontSize: 18
     }
 });
 
