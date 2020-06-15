b62b8b9cf54851b188e01caeb13183342c840fb5
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-06-15 11:41:08 +0100

    Embedding flex

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import * as Font from 'expo-font';
 import Home from './components/Home.jsx';
+import { AppLoading } from 'expo';
 
+// Gets the Nunito fonts
+const getFonts = () => Font.loadAsync({
+    'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+    'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf')
+});
 
 const App = () => {
-  return (
-    <Home />
-  );
+  const [loadedFonts, setLoadedFonts] = useState(false);
+
+  if (loadedFonts) {
+    return <Home />;
+  } else {
+    return (
+      <AppLoading 
+        startAsync={getFonts} 
+        onFinish={() => setLoadedFonts(true)} />
+    );
+  }
 }
 
 export default App;
