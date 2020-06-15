3f532d406a77677f4af8c36d8e6ac20124c09e07
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-06-15 12:46:13 +0100

    Updated App.js

--- App.js
+++ App.js
@@ -1,24 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import * as Font from 'expo-font';
 import Home from './components/Home.jsx';
 import { AppLoading } from 'expo';
 
-// Gets the Nunito fonts
 const getFonts = () => Font.loadAsync({
-    'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
-    'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf')
+  'nunito-regular': require('./assets/Fonts/Nunito-Regular.ttf'),
+  'nunito-bold': require('./assets/Fonts/Nunito-Bold.ttf')
 });
 
 const App = () => {
-  const [loadedFonts, setLoadedFonts] = useState(false);
+  const [fontsLoaded, setFontsLoaded] = useState(false);
 
-  if (loadedFonts) {
-    return <Home />;
+  if (fontsLoaded) {
+    return <Home/>;
   } else {
     return (
       <AppLoading 
-        startAsync={getFonts} 
-        onFinish={() => setLoadedFonts(true)} />
+        startAsync={getFonts}
+        onFinish={() => setFontsLoaded(true)}
+      />
     );
   }
 }
