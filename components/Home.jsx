442f38b0713cd8b56ba8e39a210e1c2a295de151
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-06-16 19:04:40 +0100

    Can now navigate between Home and Review Details components

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
 import globalStyles from '../style/globalStyles.js';
 
-const Home = () => {
+const Home = ({ navigation }) => {
+
+    // Navigates to the "Review Details" page after button press
+    const navHandler = () => navigation.push('ReviewDetails');
+
     return (
         <View style={globalStyles.container}>
             <Text style={globalStyles.titleText}>This is the Home screen</Text>
+            <Button title="Go to Review Details" onPress={navHandler} />
         </View>
     );
 }
--- components/ReviewDetails.jsx
+++ components/ReviewDetails.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
 import globalStyles from '../style/globalStyles.js';
 
-const ReviewDetails = () => {
+const ReviewDetails = ({ navigation }) => {
+
+    // Navigates back to the previous page that the user was on
+    const navHandler = () => navigation.goBack();
+
     return (
         <View style={globalStyles.container}>
             <Text style={globalStyles.titleText}>This is the Review Details Screen</Text>
+            <Button title="Back to Home Screen"  onPress={navHandler}/>
         </View>
     )
 }
