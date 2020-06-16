db3399d2db17ca81c55ab766cc78e917650fd4a9
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-06-16 19:32:08 +0100

    Component now displays each film and their reviews

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,16 +1,43 @@
-import React from 'react';
-import { Text, View, Button } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, FlatList, TouchableOpacity } from 'react-native';
 import globalStyles from '../style/globalStyles.js';
 
 const Home = ({ navigation }) => {
+
+    const [reviews, setReviews] = useState([
+        { 
+            title: "Avengers: Infinity War", 
+            rating: 9, 
+            body: "The Avengers and their allies must be willing to sacrifice all in an attempt to defeat the powerful Thanos.",
+            key: 1 
+        },
+        { 
+            title: "The Dark Knight Rises", 
+            rating: 8, 
+            body: "Eight years after the Joker's reign of anarchy, Batman, with the help of the enigmatic Catwoman, is forced from his exile to save Gotham City from the brutal guerrilla terrorist Bane.",
+            key: 2
+        },
+        { 
+            title: "Joker", 
+            rating: 9, 
+            body: "In Gotham City, mentally troubled comedian Arthur Fleck is disregarded and mistreated by society...",
+            key: 3 
+        },
+    ]);
 
     // Navigates to the "Review Details" page after button press
     const navHandler = () => navigation.push('ReviewDetails');
 
     return (
         <View style={globalStyles.container}>
-            <Text style={globalStyles.titleText}>This is the Home screen</Text>
-            <Button title="Go to Review Details" onPress={navHandler} />
+            <FlatList 
+                data={reviews}
+                renderItem={({item}) => (
+                    <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item)}>
+                        <Text style={globalStyles.titleText}>{ item.title }</Text>
+                    </TouchableOpacity>
+                )}
+            />
         </View>
     );
 }
