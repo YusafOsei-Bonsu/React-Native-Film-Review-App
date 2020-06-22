e75f6cfa5dc33996d3b6464f99295dd9dd98b6ed
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-06-22 22:53:03 +0100

    Replaced content with a single text

--- components/ReviewDetails.jsx
+++ components/ReviewDetails.jsx
@@ -1,19 +1,12 @@
 import React from 'react';
-import { Text, View, Button } from 'react-native';
+import { Text, View } from 'react-native';
 import globalStyles from '../style/globalStyles.js';
 
-const ReviewDetails = ({ route }) => {
-
-    // Navigates back to the previous page that the user was on
-    // const navHandler = () => navigation.goBack();
-
-    const { title, rating, body } = route.params;
+const ReviewDetails = ({ navigation }) => {
 
     return (
         <View style={globalStyles.container}>
-            <Text>{ title }</Text>
-            <Text>{ rating }</Text>
-            <Text>{ body }</Text>
+            <Text>This is the Review Details page</Text>
         </View>
     )
 }
