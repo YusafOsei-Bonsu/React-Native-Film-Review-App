0088a0e05d63d59dab44593cd032975e68728859
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-06-23 21:36:23 +0100

    Update ReviewDetails.jsx

--- components/ReviewDetails.jsx
+++ components/ReviewDetails.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, Button } from 'react-native';
+import { Text, View } from 'react-native';
 import Card from '../shared/Card.jsx';
 import globalStyles from '../style/globalStyles.js';
 
