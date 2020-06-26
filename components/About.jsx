8dcaed7fa16e5d6ba44277deb7a1c859f2c0d0df
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-06-26 11:19:09 +0100

    Rewriting About text

--- components/About.jsx
+++ components/About.jsx
@@ -5,7 +5,7 @@ import globalStyles from '../style/globalStyles.js';
 const About = () => {
     return (
         <View style={globalStyles.container}>
-            <Text style={globalStyles.titleText}>About Screen</Text>
+            <Text style={globalStyles.titleText}>This is a React Native-based film review app.</Text>
         </View>
     )
 }
