4f0df45f63412ddca5feb41fb2c3de29e9b99365
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-06-26 10:45:38 +0100

    Creating and styling crimson real-time errors

--- components/ReviewForm.jsx
+++ components/ReviewForm.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { StyleSheet, Button, TextInput, Text, View } from 'react-native'
+import { Button, Text, TextInput, View } from 'react-native'
 import globalStyles from '../style/globalStyles.js';
 import { Formik } from 'formik';
 import * as yup from 'yup';
 
-// For validation
+// For validation of ratings
 const reviewSchema = yup.object({
     title: yup.string().required().min(4),
     body: yup.string().required().min(8),
@@ -33,7 +33,11 @@ const ReviewForm = ({ addReview }) => {
                             placeholder="Review title"
                             onChangeText={props.handleChange("title")}
                             value={props.values.title}
+                            // Shows the error in real-time
+                            onBlur={props.handleBlur("title")}
                         />
+                        {/* Shows the minimum amount of letters that should be written */}
+                        <Text style={globalStyles.errorText}>{ props.touched.title && props.errors.title }</Text>
 
                         {/* The review written by the user */}
                         <TextInput
@@ -42,17 +46,25 @@ const ReviewForm = ({ addReview }) => {
                             placeholder="Review body"
                             onChangeText={props.handleChange("body")}
                             value={props.values.body}
+                            // Shows the error in real-time
+                            onBlur={props.handleBlur("body")}
                         />
+                        {/* Shows the minimum amount of letters that should be written */}
+                        <Text style={globalStyles.errorText}>{ props.touched.body && props.errors.body }</Text>
 
                         {/* The rating out of 5 */}
                         <TextInput
-                            multiline 
                             style={globalStyles.input}
                             placeholder="Rating (1-5)"
                             onChangeText={props.handleChange("rating")}
                             value={props.values.rating}
                             keyboardType="numeric"
+                            // Shows the error in real-time
+                            onBlur={props.handleBlur("rating")}
                         />
+                        {/* Shows the numbers that's allowed to be entered */}
+                        <Text style={globalStyles.errorText}>{ props.touched.rating && props.errors.rating }</Text>
+
                         {/* Upon being pressed, a new review is added */}
                         <Button title="Submit" color="maroon" onPress={props.handleSubmit} />
                     </View>
